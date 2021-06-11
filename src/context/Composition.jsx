e8066e1109364b2c537a2ae49e0bb585ecce4184
Chen Juanjuan <juanjuan.chen@rea-group.com>
2021-06-11 17:39:07 +0800

    Context practice

---
+++ src/context/Composition.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+const BaseComponent = (props) => {
+  return (
+    <div className={props.color}>
+      {props.children}
+    </div>
+  );
+}
+
+const SecondComponent = (props) => {
+  return (
+    <BaseComponent color="blue">
+      <h1 className="Dialog-title">
+        {props.title}
+      </h1>
+      <p className="Dialog-message">
+        {props.message}
+      </p>
+      {props.children}
+    </BaseComponent>
+  );
+}
+
+class Composition extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+    this.state = {login: ''};
+  }
+
+  render() {
+    return (
+      <SecondComponent title="Mars Exploration Program"
+              message="How should we refer to you?">
+        <input value={this.state.login}
+               onChange={this.handleChange} />
+        <button onClick={this.handleSignUp}>
+          Sign Me Up!
+        </button>
+      </SecondComponent>
+    );
+  }
+
+  handleChange(e) {
+    this.setState({login: e.target.value});
+  }
+
+  handleSignUp() {
+    alert(`Welcome aboard, ${this.state.login}!`);
+  }
+}
+
+export default Composition;
